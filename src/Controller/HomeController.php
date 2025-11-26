a3b04ef65dd19f222efb120f783da03d4cfd1184
IvanVeltz <ivan?veltz@live.fr>
2025-11-26 15:24:21 +0100

    Benniere homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,25 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        // Définit les items de navigation
-        $navItems = [
-            ['path' => 'app_home', 'label' => 'Home', 'icon' => 'trophy'],
-            ['path' => 'app_league', 'label' => "Shaquille O'League", 'icon' => 'trophy'],
-            ['path' => 'app_cup', 'label' => "Shaquille O'Cup", 'icon' => 'cup'],
-            ['path' => 'app_solo', 'label' => "Shaquille O'Solo", 'icon' => 'user'],
-        ];
+        $teamLogos = [];
 
-        return $this->render('base.html.twig', [
-            'navItems' => $navItems
+        $finder = new Finder();
+        $finder->files()->in(__DIR__ . '/../../public/images/logos');
+
+        foreach ($finder as $file) {
+            $teamLogos[] = 'images/logos/' . $file->getFilename();
+        }
+
+        return $this->render('home/index.html.twig', [
+            'teamLogos' => $teamLogos,
         ]);
     }
+
 }
